807536e3bbbef8d202f0f5e5181f8c2308353d38
Saidgani Usmonov <121709409+SaidganiUsm@users.noreply.github.com>
2023-01-19 22:29:52 +0500

    Add files via upload

---
+++ src/main/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,28 @@
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+public class RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode list = head;
+
+        while(list.next != null) {
+
+            if (list.val == list.next.val){
+
+                list.next = list.next.next;
+            }else {
+
+                list = list.next;
+            }
+
+        }
+        return head;
+    }
+}
